6df551aaad8fa04442177f4aa4ce8ffe368cbf70
jiujiteiro <romeiroe@gmail.com>
2022-01-07 14:54:26 -0300

    RP-1102, support for inplace store migrations

---
+++ x/asset/keeper/migrations.go
@@ -0,0 +1,16 @@
+package keeper
+
+// Migrator is a struct for handling in-place store migrations.
+type Migrator struct {
+	keeper Keeper
+}
+
+// NewMigrator returns a new Migrator.
+func NewMigrator(keeper Keeper) Migrator {
+	return Migrator{keeper: keeper}
+}
+
+// Migrate1to2 migrates from version 1 to 2.
+// stub example for future use
+//func (m Migrator) Migrate1to2(ctx sdk.Context) error {
+//}
