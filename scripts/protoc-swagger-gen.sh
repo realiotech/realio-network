8386dc8c1962e023d8e30b9208d70efead37ed58
jiujiteiro <romeiroe@gmail.com>
2022-12-07 15:19:20 -0500

    proto updates, gen scripts, buf files

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -3,22 +3,36 @@
 set -eo pipefail
 
 mkdir -p ./tmp-swagger-gen
-proto_dirs=$(find ./proto ./third_party/proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
 
+cd proto
+echo "Generate realio swagger files"
+proto_dirs=$(find ./ -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
   # generate swagger files (filter query files)
   query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
-  # TODO: migrate to `buf build`
   if [[ ! -z "$query_file" ]]; then
-    buf protoc  \
-    -I "proto" \
-    -I "third_party/proto" \
-    "$query_file" \
-    --swagger_out=./tmp-swagger-gen \
-    --swagger_opt=logtostderr=true --swagger_opt=fqn_for_swagger_name=true --swagger_opt=simple_operation_ids=true
+    echo "$query_file"
+    buf generate --template buf.gen.swagger.yaml "$query_file"
   fi
 done
 
+cd ../third_party/proto
+
+echo "Generate third_party swagger files"
+
+proto_dirs=$(find ./cosmos ./ethermint ./ibc -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  # generate swagger files (filter query files)
+  query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
+  if [[ ! -z "$query_file" ]]; then
+    echo "$query_file"
+    buf generate --template buf.gen.swagger.yaml "$query_file"
+  fi
+done
+
+cd ../..
+
+echo "Combine swagger files"
 # combine swagger files
 # uses nodejs package `swagger-combine`.
 # all the individual swagger files need to be configured in `config.json` for merging
@@ -27,5 +41,11 @@ swagger-combine ./client/docs/config.json -o ./client/docs/swagger-ui/swagger.ya
 # clean swagger files
 rm -rf ./tmp-swagger-gen
 
+echo "Update statik data"
+install_statik() {
+  go install github.com/rakyll/statik@v0.1.7
+}
+install_statik
+
 # generate binary for static server
-statik -src=./client/docs/swagger-ui -dest=./client/docs
+statik -f -src=./client/docs/swagger-ui -dest=./client/docs
