5ada39d4e748cf8ab587930ad9c74dbb6ed0e3da
expertdicer <expertdicer@gmail.com>
2024-04-01 11:48:37 +0700

    add log

--- x/asset/keeper/restrictions.go
+++ x/asset/keeper/restrictions.go
@@ -42,7 +42,7 @@ func (k Keeper) AssetSendRestriction(ctx sdk.Context, fromAddr, toAddr sdk.AccAd
 		if isAuthorizedFrom && isAuthorizedTo {
 			continue
 		} else { //nolint:revive // superfluous else, could fix, but not worth it?
-			err = errorsmod.Wrapf(types.ErrNotAuthorized, "%s is not authorized to transact with %s", fromAddr, coin.Denom)
+			err = errorsmod.Wrapf(types.ErrNotAuthorized, "%s is not authorized to send coin to %s with %s", fromAddr, toAddr, coin.Denom)
 			break
 		}
 	}
