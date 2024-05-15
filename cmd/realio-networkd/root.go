bd46ff901b7980634d9d754ebc6822f2d3aaec1a
GnaD13 <cpt.meoz@gmail.com>
2024-05-15 12:45:42 +0700

    add chain id when initialize app

--- cmd/realio-networkd/root.go
+++ cmd/realio-networkd/root.go
@@ -281,6 +281,7 @@ func (a appCreator) newApp(logger log.Logger, db dbm.DB, traceStore io.Writer, a
 		baseapp.SetSnapshot(snapshotStore, snapshotOptions),
 		baseapp.SetIAVLCacheSize(cast.ToInt(appOpts.Get(server.FlagIAVLCacheSize))),
 		baseapp.SetIAVLDisableFastNode(cast.ToBool(appOpts.Get(server.FlagDisableIAVLFastNode))),
+		baseapp.SetChainID(chainID),
 	)
 }
 
