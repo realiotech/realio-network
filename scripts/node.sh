448a47de707056ca08768073dc8386e3bdb37caa
GnaD13 <cpt.meoz@gmail.com>
2024-06-04 16:57:39 +0700

    nit

--- scripts/node.sh
+++ scripts/node.sh
@@ -3,7 +3,7 @@ git checkout test/upgrade
 ./local_node.sh
 
 # proposal upgrade
-realio-networkd tx gov submit-legacy-proposal software-upgrade multistaking --upgrade-height 50 --description "testing, testing, 1, 2, 3" --upgrade-info "testing" --title "Test Proposal" --from dev0 --keyring-backend test --no-validate --chain-id realionetworklocal_7777-1 --deposit 10000000ario --yes --home "$HOME/.realio-network-tmp" --fees 1000000000000000000000000ario --gas 1000000
+realio-networkd tx gov submit-legacy-proposal software-upgrade multistaking --upgrade-height 30 --description "testing, testing, 1, 2, 3" --upgrade-info "testing" --title "Test Proposal" --from dev0 --keyring-backend test --no-validate --chain-id realionetworklocal_7777-1 --deposit 10000000ario --yes --home "$HOME/.realio-network-tmp" --fees 1000000000000000000000000ario --gas 1000000
 
 # vote
 realio-networkd tx gov vote 1 yes --from dev0 --keyring-backend test --chain-id realionetworklocal_7777-1 --yes --home "$HOME/.realio-network-tmp" --fees 1000000000000000000000000ario
@@ -22,7 +22,7 @@ make install
 # run node
 realio-networkd start --log_level "info" --minimum-gas-prices=0.00001ario --json-rpc.api eth,txpool,personal,net,debug,web3 --api.enable --home "$HOME/.realio-network-tmp"
 
-realio-networkd tx gov submit-legacy-proposal software-upgrade v4 --upgrade-height 80 --description "testing, testing, 1, 2, 3" --upgrade-info "testing" --title "Test Proposal" --from dev0 --keyring-backend test --no-validate --chain-id realionetworklocal_7777-1 --deposit 10000000ario --yes --home "$HOME/.realio-network-tmp" --fees 1000000000000000000000000ario --gas 1000000
+realio-networkd tx gov submit-legacy-proposal software-upgrade v2 --upgrade-height 80 --description "testing, testing, 1, 2, 3" --upgrade-info "testing" --title "Test Proposal" --from dev0 --keyring-backend test --no-validate --chain-id realionetworklocal_7777-1 --deposit 10000000ario --yes --home "$HOME/.realio-network-tmp" --fees 1000000000000000000000000ario --gas 1000000
 
 realio-networkd tx gov vote 2 yes --from dev0 --keyring-backend test --chain-id realionetworklocal_7777-1 --yes --home "$HOME/.realio-network-tmp" --fees 1000000000000000000000000ario
 
