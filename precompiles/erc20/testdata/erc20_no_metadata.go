bb778f2aeda836d4bbd5d9539119c4d51386c0f2
Hieu Vu <hieuvubk@gmail.com>
2025-02-23 14:39:40 +0700

    fork erc20 precompiles

---
+++ precompiles/erc20/testdata/erc20_no_metadata.go
@@ -0,0 +1,13 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+
+package testdata
+
+import (
+	contractutils "github.com/evmos/os/contracts/utils"
+	evmtypes "github.com/evmos/os/x/evm/types"
+)
+
+func LoadERC20NoMetadataContract() (evmtypes.CompiledContract, error) {
+	return contractutils.LoadContractFromJSONFile("ERC20NoMetadata.json")
+}
