eacb938cc97e07046ddd6a5e99c8a92115b73dd7
Hieu Vu <hieuvubk@gmail.com>
2025-07-28 23:25:24 +0700

    no need

--- app/upgrades/v1.4/upgrades.go
+++ app/upgrades/v1.4/upgrades.go
@@ -6,8 +6,6 @@ import (
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	feemarkettypes "github.com/cosmos/evm/x/feemarket/types"
-	vmtypes "github.com/cosmos/evm/x/vm/types"
 )
 
 // CreateUpgradeHandler creates an SDK upgrade handler for v1.3.0
@@ -19,8 +17,6 @@ func CreateUpgradeHandler(
 		sdkCtx := sdk.UnwrapSDKContext(ctx)
 		sdkCtx.Logger().Info("Starting upgrade for v1.4.0...")
 
-		vm[vmtypes.ModuleName] = 8
-		vm[feemarkettypes.ModuleName] = 5
 		return mm.RunMigrations(ctx, cfg, vm)
 	}
 }
