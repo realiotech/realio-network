6a122cd706d3a1faf122ff2a2f8c6376b93616b0
Hieu Vu <hieuvubk@gmail.com>
2025-09-18 17:26:36 +0700

    update types

--- precompile/multistaking/types.go
+++ precompile/multistaking/types.go
@@ -139,6 +139,15 @@ func NewValidatorRequest(args []interface{}) (*multistakingtypes.QueryValidatorR
 	return &multistakingtypes.QueryValidatorRequest{ValidatorAddr: validatorAddress}, nil
 }
 
+// Description use golang type alias defines a validator description.
+type Description = struct {
+	Moniker         string "json:\"moniker\""
+	Identity        string "json:\"identity\""
+	Website         string "json:\"website\""
+	SecurityContact string "json:\"securityContact\""
+	Details         string "json:\"details\""
+}
+
 // ValidatorInfo is a struct to represent the key information from
 // a validator response.
 type ValidatorInfo struct {
@@ -148,7 +157,7 @@ type ValidatorInfo struct {
 	Status            uint8    `abi:"status"`
 	Tokens            *big.Int `abi:"tokens"`
 	DelegatorShares   *big.Int `abi:"delegatorShares"` // TODO: Decimal
-	Description       string   `abi:"description"`
+	Description       stakingtypes.Description   `abi:"description"`
 	UnbondingHeight   int64    `abi:"unbondingHeight"`
 	UnbondingTime     int64    `abi:"unbondingTime"`
 	Commission        *big.Int `abi:"commission"`
@@ -170,7 +179,7 @@ func DefaultValidatorOutput() ValidatorOutput {
 			Status:            uint8(0),
 			Tokens:            big.NewInt(0),
 			DelegatorShares:   big.NewInt(0),
-			Description:       "",
+			Description:       stakingtypes.Description{},
 			UnbondingHeight:   int64(0),
 			UnbondingTime:     int64(0),
 			Commission:        big.NewInt(0),
@@ -195,7 +204,7 @@ func (vo *ValidatorOutput) FromResponse(res *multistakingtypes.QueryValidatorRes
 			Status:            uint8(stakingtypes.BondStatus_value[res.Validator.Status.String()]), //#nosec G115 // enum will always be convertible to uint8
 			Tokens:            res.Validator.Tokens.BigInt(),
 			DelegatorShares:   res.Validator.DelegatorShares.BigInt(),
-			Description:       res.Validator.Description.Details,
+			Description:       res.Validator.Description,
 			UnbondingHeight:   res.Validator.UnbondingHeight,
 			UnbondingTime:     res.Validator.UnbondingTime.UTC().Unix(),
 			Commission:        res.Validator.Commission.CommissionRates.Rate.BigInt(),
