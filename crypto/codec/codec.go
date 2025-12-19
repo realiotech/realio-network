6129b324ee50ac4c6dee9839103a8a24d2cc4b88
Freddy Caceres <facs95@gmail.com>
2025-12-18 22:12:21 -0500

    chore: add falback decoder

--- crypto/codec/codec.go
+++ crypto/codec/codec.go
@@ -8,8 +8,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 
-	"github.com/cosmos/cosmos-sdk/types/tx"
-	"github.com/cosmos/evm/x/vm/types/legacy"
 	"github.com/realiotech/realio-network/crypto/account"
 	"github.com/realiotech/realio-network/crypto/ethsecp256k1"
 	"github.com/realiotech/realio-network/crypto/ossecp256k1"
@@ -29,29 +27,4 @@ func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*cryptotypes.PrivKey)(nil), &ethsecp256k1.PrivKey{})
 	registry.RegisterImplementations((*cryptotypes.PubKey)(nil), &ossecp256k1.PubKey{})
 	registry.RegisterImplementations((*cryptotypes.PrivKey)(nil), &ossecp256k1.PrivKey{})
-
-	// Support /os.evm.v1.MsgEthereumTx (legacy format from cosmos/evm)
-	registry.RegisterImplementations(
-		(*tx.TxExtensionOptionI)(nil),
-		&legacy.ExtensionOptionsEthereumTx{},
-	)
-	registry.RegisterImplementations(
-		(*sdk.Msg)(nil),
-		&legacy.MsgEthereumTx{},
-		&legacy.MsgUpdateParams{},
-	)
-	registry.RegisterInterface(
-		"ethermint.evm.v1.TxData",
-		(*legacy.TxData)(nil),
-		&legacy.DynamicFeeTx{},
-		&legacy.AccessListTx{},
-		&legacy.LegacyTx{},
-	)
-	registry.RegisterInterface(
-		"os.evm.v1.TxData",
-		(*legacy.TxData)(nil),
-		&legacy.DynamicFeeTx{},
-		&legacy.AccessListTx{},
-		&legacy.LegacyTx{},
-	)
 }
