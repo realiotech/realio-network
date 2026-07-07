13a3a7201724b5373481500a1bb11226e5632603
Hieu Vu <hieuvubk@gmail.com>
2026-07-07 16:53:02 +0700

    store keys ibcwasm

--- app/upgrades/v1.7/constants.go
+++ app/upgrades/v1.7/constants.go
@@ -4,6 +4,7 @@ import (
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 
 	storetypes "cosmossdk.io/store/types"
+	ibcwasmtypes "github.com/cosmos/ibc-go/modules/light-clients/08-wasm/v10/types"
 )
 
 const (
@@ -14,5 +15,6 @@ const (
 var V7StoreUpgrades = storetypes.StoreUpgrades{
 	Added: []string{
 		wasmtypes.ModuleName,
+		ibcwasmtypes.ModuleName,
 	},
 }
